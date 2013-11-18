3e315f90a3c2b573daa62a1e6ba93dc16e2d2c4d
supernun <jad.knayzeh@gmail.com>
2013-11-18 18:53:55 -0500

    Light source DIFFUSE_AMBIENT now supported

--- a3/raytracerLinux/light_source.cpp
+++ a3/raytracerLinux/light_source.cpp
@@ -11,7 +11,13 @@
 #include <cmath>
 #include "light_source.h"
 
-RenderType LightSource::RENDER_TYPE = SCENE_SIGNATURE;
+//RenderType LightSource::RENDER_TYPE = SCENE_SIGNATURE;
+RenderType LightSource::RENDER_TYPE = DIFFUSE_AMBIENT; 
+
+
+double dmax(double a, double b) {
+    return a < b ? b : a;
+}
 
 void PointLight::shade( Ray3D& ray ) {
 	// TODO: implement this function to fill in values for ray.col 
@@ -21,10 +27,24 @@ void PointLight::shade( Ray3D& ray ) {
 	// It is assumed at this point that the intersection information in ray 
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
+	
+    Vector3D normal_vector = ray.intersection.normal;
+    Vector3D light_vector = _pos - Point3D(0,0,0);
+    normal_vector.normalize();
+    light_vector.normalize();
+
 	if (LightSource::RENDER_TYPE == SCENE_SIGNATURE) {
-			ray.col = ray.intersection.mat->diffuse;
-		} else {
-			// TODO:
+        ray.col = ray.intersection.mat->diffuse;
 	}
+    else if (LightSource::RENDER_TYPE == DIFFUSE_AMBIENT) {
+        ray.col =  ray.intersection.mat->ambient * _col_ambient + dmax(0,normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse;
+    }
+    else if (LightSource::RENDER_TYPE == DIFFUSE_AMBIENT) {
+        
+
+    }
+
+
+			// TODO:
 }
 
