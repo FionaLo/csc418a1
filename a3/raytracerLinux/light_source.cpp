d04abb18b289cce5ef5cfd921481a330161ec7b6
supernun <jad.knayzeh@gmail.com>
2013-11-18 19:29:40 -0500

    Phong model added (not working properly)

--- a3/raytracerLinux/light_source.cpp
+++ a3/raytracerLinux/light_source.cpp
@@ -12,7 +12,8 @@
 #include "light_source.h"
 
 //RenderType LightSource::RENDER_TYPE = SCENE_SIGNATURE;
-RenderType LightSource::RENDER_TYPE = DIFFUSE_AMBIENT; 
+//RenderType LightSource::RENDER_TYPE = DIFFUSE_AMBIENT; 
+RenderType LightSource::RENDER_TYPE = PHONG; 
 
 
 double dmax(double a, double b) {
@@ -27,24 +28,34 @@ void PointLight::shade( Ray3D& ray ) {
 	// It is assumed at this point that the intersection information in ray 
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
+
+    // TODO:
 	
     Vector3D normal_vector = ray.intersection.normal;
     Vector3D light_vector = _pos - Point3D(0,0,0);
+    double ldotn = light_vector.dot(normal_vector);
+    Vector3D reflect_vector = 2 * ldotn * normal_vector - light_vector;
+    Vector3D view_vector = -ray.dir;
+
     normal_vector.normalize();
     light_vector.normalize();
+    reflect_vector.normalize();
+    view_vector.normalize();
+
 
 	if (LightSource::RENDER_TYPE == SCENE_SIGNATURE) {
         ray.col = ray.intersection.mat->diffuse;
 	}
     else if (LightSource::RENDER_TYPE == DIFFUSE_AMBIENT) {
-        ray.col =  ray.intersection.mat->ambient * _col_ambient + dmax(0,normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse;
+        ray.col =  ray.intersection.mat->ambient * _col_ambient + 
+                    dmax(0,normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse;
     }
-    else if (LightSource::RENDER_TYPE == DIFFUSE_AMBIENT) {
-        
-
+    else if (LightSource::RENDER_TYPE == PHONG) {
+        ray.col =  ray.intersection.mat->ambient * _col_ambient + 
+                    dmax(0, normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse +
+                    dmax(0, pow(view_vector.dot(reflect_vector),ray.intersection.mat->specular_exp)) * ray.intersection.mat->specular * _col_specular;
     }
 
 
-			// TODO:
 }
 
