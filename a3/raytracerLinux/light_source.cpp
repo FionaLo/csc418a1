b299bc6151d140b462f8e0e7e9a3a68a9c365d1a
newmann <newman-n@hotmail.com>
2013-11-19 21:22:48 -0800

    Fixes bugs - clamp colour so values stay in proper ranges. Fix light_vector so it is the vector from the intersection point

--- a3/raytracerLinux/light_source.cpp
+++ a3/raytracerLinux/light_source.cpp
@@ -32,7 +32,7 @@ void PointLight::shade( Ray3D& ray ) {
     // TODO:
 	
     Vector3D normal_vector = ray.intersection.normal;
-    Vector3D light_vector = _pos - Point3D(0,0,0);
+    Vector3D light_vector = _pos - ray.intersection.point;
     double ldotn = light_vector.dot(normal_vector);
     Vector3D reflect_vector = 2 * ldotn * normal_vector - light_vector;
     Vector3D view_vector = -ray.dir;
@@ -42,20 +42,24 @@ void PointLight::shade( Ray3D& ray ) {
     reflect_vector.normalize();
     view_vector.normalize();
 
-
-	if (LightSource::RENDER_TYPE == SCENE_SIGNATURE) {
-        ray.col = ray.intersection.mat->diffuse;
-	}
-    else if (LightSource::RENDER_TYPE == DIFFUSE_AMBIENT) {
-        ray.col =  ray.intersection.mat->ambient * _col_ambient + 
-                    dmax(0,normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse;
+    Colour col;
+    switch(LightSource::RENDER_TYPE) {
+        case SCENE_SIGNATURE:
+            col = ray.intersection.mat->diffuse;
+        break;
+        case DIFFUSE_AMBIENT:
+            col = ray.intersection.mat->ambient * _col_ambient + 
+                  dmax(0,normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse;
+        break;
+        case PHONG:
+            col = ray.intersection.mat->ambient * _col_ambient + 
+                  dmax(0, normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse +
+                  dmax(0, pow(view_vector.dot(reflect_vector),ray.intersection.mat->specular_exp)) * ray.intersection.mat->specular * _col_specular;
+        break;
+        default:
+            throw 1;
     }
-    else if (LightSource::RENDER_TYPE == PHONG) {
-        ray.col =  ray.intersection.mat->ambient * _col_ambient + 
-                    dmax(0, normal_vector.dot(light_vector)) * ray.intersection.mat->diffuse * _col_diffuse +
-                    dmax(0, pow(view_vector.dot(reflect_vector),ray.intersection.mat->specular_exp)) * ray.intersection.mat->specular * _col_specular;
-    }
-
-
+    col.clamp();
+    ray.col = ray.col + col;
 }
 
